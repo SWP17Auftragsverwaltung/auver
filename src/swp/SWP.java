1a145ee459d42fa1f19456442fb8c28ac470b17b
AnDruXa90 <andrej-hensel@arcor.de>
2017-06-08 18:33:34 +0200

    Create SWP.java

--- src/swp/SWP.java
+++ src/swp/SWP.java
@@ -11,6 +11,7 @@ public class SWP {
     public static void main(String[] args) {
         // TODO code application logic here
         //TEST TEST TEST
+        //TEST" TEST " TESTET+#zrfasdgök#nasgnagdlöfgb
     }
     
 }
